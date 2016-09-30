4215f29134fe16f5c4aab3d7bbfd466e544c3489
Beylul <bkbreab8@gmail.com>
2016-09-30 13:57:04 -0700

    Added list of tasks and one task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  def index
+    @task_lists = [
+        {name: "homework",
+        description: "read rails documents",
+        completion_status: "yes",
+        completion_status_date: "9/28/26"
+      },
+        {name: "cleaning",
+        description: "clean room",
+        completion_status: "no",
+        completion_status_date: "N/A"},
+
+        {name: "cook",
+        description: "cook dinner",
+        completion_status: "halfway",
+        completion_status_date: "N/A"},
+      ]
+  end
+
+  def show
+    @task = [
+        {name: "homework",
+        description: "read rails documents",
+        completion_status: "yes",
+        completion_status_date: "9/27/2016"}]
+  end
+
+
+
 end
