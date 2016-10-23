3b3d8f7079dcd5296070ce71557100041c6b001c
Beylul <bkbreab8@gmail.com>
2016-10-23 16:03:29 -0700

    Done with Phase one and Half of Phase 2, ensured that non authenticated users can only see the welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:login, :create, :index]
+
+  def index
+      @user = User.find(session[:user_id])
+    end
+
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
+    redirect_to login_failure_path unless auth_hash['uid']
+    @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+
+    if @user.nil?
+      @user = User.build_from_github(auth_hash)
+      render :creation_failure unless @user.save
+    end
+   session[:user_id] = @user.id
+    redirect_to sessions_path
+  end
+
+  def login; end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+ root 'sessions#login'
   get '/tasks' => 'tasks#index'
   get '/tasks/new' => 'tasks#new'
   post '/tasks' => 'tasks#create'
@@ -8,60 +8,10 @@ Rails.application.routes.draw do
   patch '/tasks/:id/completed', to: 'tasks#completed', as: 'task_completed'
   patch '/tasks/:id' => 'tasks#update'
   delete 'tasks/:id' =>'tasks#destroy'
+
   get "/auth/:provider/callback" =>  "sessions#create"
+  get "/sessions/login_failure", to: "sessions#login_failure", as: "login_failure"
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get "/sessions", to: "sessions#index", as: "sessions"
+  delete "/sessions", to: "sessions#destroy"
 end
