ce17e3734f48b17f155149e22769b333d6e4310f
Beylul <bkbreab8@gmail.com>
2016-10-23 23:44:03 -0700

    added tests to the user_test.rb and added fixtures to user.yml

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Users with all info provided are valid" do
+    assert users(:hyab).valid?
+  end
+
+  test "Users with missing e-mail are invalid" do
+     hnu = User.create(
+      name: "Hyab Hnu",
+      uid: 88888,
+      provider: "github")
+     User.create
+    assert_not hnu.valid?
+  end
+
+  test "Users with missing uid are invalid" do
+     hnu = User.create(
+      name: "Hyab Hnu",
+      email: "hnu@gmail.com",
+      provider: "github")
+     User.create
+    assert_not hnu.valid?
+  end
+  test "Users with missing provider are invalid" do
+     hnu = User.create(
+      name: "Hyab Hnu",
+      email: "hnu@gmail.com",
+      uid: 88888,)
+     User.create
+    assert_not hnu.valid?
+  end
+  test "Users with missing name are valid" do
+     hnu = User.create(
+      email: "hnu@gmail.com",
+      uid: 88888,
+      provider: "github")
+     User.create
+    assert hnu.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,13 @@ require 'simplecov'
 SimpleCov.start
 Rails.application.eager_load!
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   Minitest::Reporters.use!
-  # Add more helper methods to be used by all tests here...
+  def setup
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github', uid: '99999', info: { email: "a@b.com", name: "Ada" }
+      })
+  end
+
 end
